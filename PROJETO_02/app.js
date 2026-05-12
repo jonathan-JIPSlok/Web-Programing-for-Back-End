5e58e3b4e3d1be1e08ca0437855f880b48762ae4
jonathan-jipslok <jonathanpoli17@gmail.com>
2026-05-11 21:14:37 -0300

    feat: instalando prisma e adicionado uma tablea no banco de dados mysql

---
+++ PROJETO_02/app.js
@@ -0,0 +1,28 @@
+/********************************************************************
+ * Objetivo: API para realizar requisições de contatos da UniFecaf
+ * Data: 11/05/2026
+ * Autor: Jonathan Yuri
+ * Versão: 1.0
+ *********************************************************************/
+
+//MVC - (model, view, controller)
+
+/*
+    model - Modelagem dos dados (Banco de Dados)
+    Controller - é responsável pela regra de negócio do projeto
+    view - é responsável pera interação com o cliente (usuário)
+
+    instalamos o express, cors e bod-parser
+
+    Dependencias para acesso a banco de dados
+    - Sequelize
+    - Prisma (Vamos usar aqui)
+    - Fastfy
+
+    Instalação e configuração do prisma
+    npm install prisma --save
+    npx prisma init
+    npm install @prisma/client --save
+
+*/
+
---
+++ PROJETO_02/prisma/migrations/20260512001147_tbl_contatos/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `tbl_contatos` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(80) NOT NULL,
+    `cpf` VARCHAR(18) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+
+    UNIQUE INDEX `id`(`id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
